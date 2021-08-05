a3edab741fe6b932cef62572ccaa96c76a9f5d5e
f-blan <c.blangio@gmail.com>
2021-08-05 14:06:12 +0200

    added a sample game object

--- Assets/Scripts/Hello.cs
+++ Assets/Scripts/Hello.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Hello : MonoBehaviour
 {
+    private float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(1,0,0);
+        transform.localScale = new Vector3(3,3,3);
+        speed = 0.01f;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if(transform.position[0] >= 4f){
+            Debug.Log("switch left");
+            speed = -0.01f;
+        }else if (transform.position[0]<= -4f){
+            Debug.Log("switch right");
+            speed = 0.01f;
+        }
+        transform.position = transform.position + new Vector3(speed,0,0);
     }
 }
