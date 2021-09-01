35b0be1eea07172a9d958f9cbe02a2ba49053f25
SamuelOreste Abreu <orestsam@protonmail.com>
2021-09-01 16:57:35 +0200

    Fix: Drawing code for simplicity's sake

--- Assets/ECS/Systems/BusLineDrawingSystem.cs
+++ Assets/ECS/Systems/BusLineDrawingSystem.cs
@@ -6,46 +6,30 @@ using Unity.Mathematics;
 [UpdateAfter(typeof(BusMovementSystem))]
 public class BusLineDrawingSystem : SystemBase
 {
-    private NativeList<int2> nativeListGraph;
-    private NativeList<Vector3> nativeListWorldPoint;
-    private bool set;
 
+    EntityQueryDesc queryBusSelected = new EntityQueryDesc{All = new ComponentType[]{ComponentType.ReadOnly<SelectedVehicleTag>(), ComponentType.ReadOnly<BusPathComponent>()}};
     GameObject line;
-
-    protected override void OnStartRunning()
-    {
-        base.OnStartRunning();
-        nativeListGraph = new NativeList<int2>(Allocator.Persistent);
-        set = false;
-    }
 
     protected override void OnUpdate()
     {
-        if(!set){
-            NativeList<int2> localNativeList = nativeListGraph;
-            int verse = 1;
-            Entities.WithAll<BusPathComponent,SelectedVehicleTag>().ForEach((in BusPathComponent busPathComponent, in VehicleMovementData vehicleMovementData) => {
-                verse = busPathComponent.verse;
-                if(localNativeList.Length == 0){
-                    ref BlobArray<PathElement> arrayPath = ref busPathComponent.pathArrayReference.Value.pathArray;
-                    for(int i = 0; i < busPathComponent.pathLength; i++){
-                        localNativeList.Add(new int2(arrayPath[i].x, arrayPath[i].y));//Getting node position
-                    } 
-                    localNativeList.Add(new int2(arrayPath[0].x, arrayPath[0].y));
-                } 
-                // Debug.Log(localNativeList.Length);
-            }).Run(); //Can't be done in parallel, check how much does it affect performance
-            if(localNativeList.Length == 0){
+        if(line == null){
+            NativeArray<BusPathComponent> busPathComponents = GetEntityQuery(queryBusSelected).ToComponentDataArray<BusPathComponent>(Allocator.Temp);
+            if(busPathComponents.Length == 0 || busPathComponents[0].pathLength == 0){
                 return;
             }
+            NativeArray<Vector3> worldPoints = new NativeArray<Vector3>(busPathComponents[0].pathLength+1, Allocator.Temp);
 
-            nativeListWorldPoint = new NativeList<Vector3>(Allocator.Temp);
+            int verse = busPathComponents[0].verse;
+            // NativeList<int2> localNativeList = nativeListGraph;
 
-            for(int i = 0; i < localNativeList.Length; i++){ 
-                nativeListWorldPoint.Add(Map_Setup.Instance.CityGraph.GetGraphNode(localNativeList[i].x, localNativeList[i].y).GetReferenceTile().GetWorldPosition());
+            PathElement[] pathElements = busPathComponents[0].pathArrayReference.Value.pathArray.ToArray();
+
+            for(int i = 0; i < pathElements.Length; i++){ 
+                worldPoints[i] = Map_Setup.Instance.CityGraph.GetGraphNode(pathElements[i].x, pathElements[i].y).GetReferenceTile().GetWorldPosition();
                 // Debug.Log("Node position: " + localNativeList[i].x + "," + localNativeList[i].y);
                 // Debug.Log("World position: " + Map_Setup.Instance.CityGraph.GetGraphNode(localNativeList[i].x, localNativeList[i].y).GetReferenceTile().GetWorldPosition());
             } 
+            worldPoints[pathElements.Length] = Map_Setup.Instance.CityGraph.GetGraphNode(pathElements[0].x, pathElements[0].y).GetReferenceTile().GetWorldPosition();
 
             line = new GameObject();
             LineRenderer localDrawLine = line.AddComponent<LineRenderer>();
@@ -58,19 +42,11 @@ public class BusLineDrawingSystem : SystemBase
             localDrawLine.startColor = (verse == -1 ? Color.red : Color.blue);
             localDrawLine.endColor = localDrawLine.startColor;
 
-            localDrawLine.positionCount = localNativeList.Length; 
-            localDrawLine.SetPositions(nativeListWorldPoint.ToArray());
+            localDrawLine.positionCount = worldPoints.Length; 
+            localDrawLine.SetPositions(worldPoints.ToArray());
 
-            nativeListWorldPoint.Dispose();
-            set = true;
+            busPathComponents.Dispose();
+            worldPoints.Dispose();
         }
     }
-
-
-    protected override void OnDestroy()
-    {
-        base.OnDestroy();
-        nativeListGraph.Dispose();
-    }
-
 }
