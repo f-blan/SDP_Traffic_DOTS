104d06e731cf0033bbc7708eff5ae478498556ff
SamuelOreste Abreu <orestsam@protonmail.com>
2021-09-01 17:41:23 +0200

    Feat: Added comments to drawing code

--- Assets/ECS/Systems/BusLineDrawingSystem.cs
+++ Assets/ECS/Systems/BusLineDrawingSystem.cs
@@ -9,42 +9,43 @@ public class BusLineDrawingSystem : SystemBase
 
     EntityQueryDesc queryBusSelected = new EntityQueryDesc{All = new ComponentType[]{ComponentType.ReadOnly<SelectedVehicleTag>(), ComponentType.ReadOnly<BusPathComponent>()}};
     GameObject line;
-
     protected override void OnUpdate()
     {
+        //Do not do anything if the line object already exists
         if(line == null){
+            //Get query defined above
             NativeArray<BusPathComponent> busPathComponents = GetEntityQuery(queryBusSelected).ToComponentDataArray<BusPathComponent>(Allocator.Temp);
+            //Don't do anything if nothing was returned or if the bus has no path
             if(busPathComponents.Length == 0 || busPathComponents[0].pathLength == 0){
                 return;
             }
+            //Creating an array in which the world points will be stored, given the information obtained from the query above;
             NativeArray<Vector3> worldPoints = new NativeArray<Vector3>(busPathComponents[0].pathLength+1, Allocator.Temp);
-
-            int verse = busPathComponents[0].verse;
-            // NativeList<int2> localNativeList = nativeListGraph;
-
+            //Getting array of path elements
             PathElement[] pathElements = busPathComponents[0].pathArrayReference.Value.pathArray.ToArray();
-
+            //Mapping nodes to world points
             for(int i = 0; i < pathElements.Length; i++){ 
                 worldPoints[i] = Map_Setup.Instance.CityGraph.GetGraphNode(pathElements[i].x, pathElements[i].y).GetReferenceTile().GetWorldPosition();
-                // Debug.Log("Node position: " + localNativeList[i].x + "," + localNativeList[i].y);
-                // Debug.Log("World position: " + Map_Setup.Instance.CityGraph.GetGraphNode(localNativeList[i].x, localNativeList[i].y).GetReferenceTile().GetWorldPosition());
             } 
             worldPoints[pathElements.Length] = Map_Setup.Instance.CityGraph.GetGraphNode(pathElements[0].x, pathElements[0].y).GetReferenceTile().GetWorldPosition();
 
-            line = new GameObject();
-            LineRenderer localDrawLine = line.AddComponent<LineRenderer>();
-            line.name = "BusLineTrace";
-            localDrawLine.material = new Material(Shader.Find("Sprites/Default"));
+            //Drawing line code
+            line = new GameObject(); //Creating a blank game object
+            LineRenderer localDrawLine = line.AddComponent<LineRenderer>(); //Add line renderer component to the game object
+            line.name = "BusLineTrace"; //Setting name
+            localDrawLine.material = new Material(Shader.Find("Sprites/Default")); //Adding shader
 
+            //Setting line width
             localDrawLine.startWidth = 0.1f;
             localDrawLine.endWidth = localDrawLine.startWidth;
-
-            localDrawLine.startColor = (verse == -1 ? Color.red : Color.blue);
+            //Setting color
+            localDrawLine.startColor = (busPathComponents[0].verse == -1 ? Color.red : Color.blue);
             localDrawLine.endColor = localDrawLine.startColor;
-
+            //Setting amount of points and adding the points
             localDrawLine.positionCount = worldPoints.Length; 
             localDrawLine.SetPositions(worldPoints.ToArray());
 
+            //Disposing of arrays
             busPathComponents.Dispose();
             worldPoints.Dispose();
         }
